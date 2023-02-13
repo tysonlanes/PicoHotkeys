7dd8a26a308b0e1cf7573c4fc8f2bd0327e4cfa8
tysonlanes <58047620+tysonlanes@users.noreply.github.com>
2023-02-13 11:55:26 -0700

    Update boot.py

--- boot.py
+++ boot.py
@@ -1,3 +1,11 @@
-import storage
+import storage, usb_cdc
+import board, digitalio
 
-storage.disable_usb_drive()
+# Setup the button
+button = digitalio.DigitalInOut(board.GP16)
+button.pull = digitalio.Pull.UP
+
+# Disable devices only if button is not pressed.
+if button.value:
+	storage.disable_usb_drive()
+	usb_cdc.disable()
