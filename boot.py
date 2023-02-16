4f352c4634746714db6a015335a0fb6a22762a48
tysonlanes <58047620+tysonlanes@users.noreply.github.com>
2023-02-16 16:51:40 -0700

    Update boot.py

--- boot.py
+++ boot.py
@@ -1,11 +1,13 @@
 import storage, usb_cdc
 import board, digitalio
+import usb_midi
 
 # Setup the button
-button = digitalio.DigitalInOut(board.GP16)
+button = digitalio.DigitalInOut(board.GP3)
 button.pull = digitalio.Pull.UP
 
 # Disable devices only if button is not pressed.
 if button.value:
 	storage.disable_usb_drive()
 	usb_cdc.disable()
+	usb_midi.disable()
