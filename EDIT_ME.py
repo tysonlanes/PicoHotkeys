e22e087119ef4a4d77d98d06b6c1f27a1f953a69
tysonlanes <58047620+tysonlanes@users.noreply.github.com>
2023-02-16 20:43:46 -0700

    Update EDIT_ME.py

--- EDIT_ME.py
+++ EDIT_ME.py
@@ -4,5 +4,6 @@
 # If you need a quotation mark, precede it with a backslash - \
 # The example is copied in comments below.
 # CHANGE_ME = str("This is the \"string\" of text to type!\nAnd this is in a new line.\nThis \\ is a single backslash :)")
+# To type Hello world! would be - CHANGE_ME = str("Hello world!")
 
 CHANGE_ME = str("This is the \"string\" of text to type!\nAnd this is in a new line.\nThis \\ is a single backslash :)")
