2e08421e1d17d74cf4aee94c9ebf80c7777cb064
tysonlanes <58047620+tysonlanes@users.noreply.github.com>
2023-02-16 19:58:51 -0700

    Create EDIT_ME.py

---
+++ EDIT_ME.py
@@ -0,0 +1,8 @@
+# This is the file to edit for the red keyboard button.
+# Whatever is inside of quotation marks will be typed.
+# A \n is a new line.  Use this if you want to hit enter after your string.
+# If you need a quotation mark, precede it with a backslash - \
+# The example is copied in comments below.
+# CHANGE_ME = str("This is the \"string\" of text to type!\nAnd this is in a new line.\nThis \\ is a backslash :)")
+
+CHANGE_ME = str("This is the \"string\" of text to type!\nAnd this is in a new line.\nThis \\ is a backslash :)")
