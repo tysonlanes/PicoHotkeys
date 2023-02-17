2ef0bfe649a77d1c74d88b46ce044bb6718b8e92
tysonlanes <58047620+tysonlanes@users.noreply.github.com>
2023-02-16 20:01:38 -0700

    Update EDIT_ME.py

--- EDIT_ME.py
+++ EDIT_ME.py
@@ -3,6 +3,6 @@
 # A \n is a new line.  Use this if you want to hit enter after your string.
 # If you need a quotation mark, precede it with a backslash - \
 # The example is copied in comments below.
-# CHANGE_ME = str("This is the \"string\" of text to type!\nAnd this is in a new line.\nThis \\ is a backslash :)")
+# CHANGE_ME = str("This is the \"string\" of text to type!\nAnd this is in a new line.\nThis \\ is a single backslash :)")
 
-CHANGE_ME = str("This is the \"string\" of text to type!\nAnd this is in a new line.\nThis \\ is a backslash :)")
+CHANGE_ME = str("This is the \"string\" of text to type!\nAnd this is in a new line.\nThis \\ is a single backslash :)")
